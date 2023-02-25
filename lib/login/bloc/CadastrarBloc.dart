188af74572f41efea47af26aa7881225cb95fb52
anaclcorrea <aclc013@gmail.com>
2023-02-25 20:03:15 -0300

    Classes criadas

---
+++ lib/login/bloc/CadastrarBloc.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class CadastrarBloc {
+  var emailController = TextEditingController();
+  var senhaController = TextEditingController();
+  var nomeController = TextEditingController();
+  var telefoneController = TextEditingController();
+
+
+}
