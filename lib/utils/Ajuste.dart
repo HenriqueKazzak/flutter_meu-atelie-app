9ca03899fd05030a774579902df1719345f559b1
anaclcorrea <aclc013@gmail.com>
2023-02-27 21:25:26 -0300

    Getters e Setters

---
+++ lib/utils/Ajuste.dart
@@ -0,0 +1,11 @@
+class Ajuste{
+  int _valorTotal;
+
+  Ajuste(this._valorTotal);
+
+  int get valorTotal => _valorTotal;
+
+  set valorTotal(int value) {
+    _valorTotal = value;
+  }
+}
--- lib/utils/Costureira.dart
+++ lib/utils/Costureira.dart
@@ -1,3 +1,27 @@
-Class Costureira {
-  String
+import 'dart:ui';
+
+class Costureira {
+  String _nome;
+  String _telefone;
+  Image _fotoPerfil;
+
+  Costureira(this._nome, this._telefone, this._fotoPerfil);
+
+  String get nome => _nome;
+
+  set nome(String value) {
+    _nome = value;
+  }
+
+  String get telefone => _telefone;
+
+  Image get fotoPerfil => _fotoPerfil;
+
+  set fotoPerfil(Image value) {
+    _fotoPerfil = value;
+  }
+
+  set telefone(String value) {
+    _telefone = value;
+  }
 }
