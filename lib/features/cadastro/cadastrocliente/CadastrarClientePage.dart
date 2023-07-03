da45c1270f3bd981aaeadb546d73c942964c513e
hkarmier <henriquemendesk@gmail.com>
2023-07-03 20:30:18 -0300

    novas features

--- lib/features/cadastro/cadastrocliente/CadastrarClientePage.dart
+++ lib/features/cadastro/cadastrocliente/CadastrarClientePage.dart
@@ -11,39 +11,54 @@ class CadastrarClientePage extends StatefulWidget {
 
 class _CadastrarClientePageState extends State<CadastrarClientePage> {
   var clienteBloc = RegisterClienteBloc();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          const Text(
-              "Cliente"
-          ),
-          TextField(
-            decoration: InputDecoration(
-              hintText: "Cliente"
+      appBar: AppBar(
+        title: const Text('Cadastrar Cliente'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              "Cliente",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-            controller: clienteBloc.NomeCliente,
-          ),
-          TextField(
-            decoration: InputDecoration(
-              hintText: "Email",
+            const SizedBox(height: 20),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: "Nome",
+                border: OutlineInputBorder(),
+              ),
+              controller: clienteBloc.NomeCliente,
             ),
-            controller: clienteBloc.TelefoneCliente,
-          ),
-          TextButton(
-            onPressed: () {
-              clienteBloc.cadastrarCliente();
-              Navigator.pop(context);
-            },
-            child: const Text("Cadastrar"),
-          ),
-        ],
+            const SizedBox(height: 10),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: "Email",
+                border: OutlineInputBorder(),
+              ),
+              controller: clienteBloc.TelefoneCliente,
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                clienteBloc.cadastrarCliente();
+                Navigator.pop(context);
+              },
+              child: const Text("Cadastrar"),
+            ),
+          ],
+        ),
       ),
     );
   }
-
-
 }
+
